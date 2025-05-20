bf1b4eb4e2112200eedc1f379e15d4e8d74157f6
ISABELLE LEITE DA SILVA MAXIMO FERREIRA <24271526@cfp107_2.local>
2025-05-20 16:31:41 -0300

    API's

--- API'S/API_economia.py
+++ API'S/API_economia.py
@@ -4,20 +4,20 @@
 # dolar, euri, bitcoin
 
 import requests
-# url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL" # add EUR-BRL,BTC-BRL paea ver o euro e o bitcoin
+url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL" # add EUR-BRL,BTC-BRL paea ver o euro e o bitcoin
 
-# response = requests.get(url)
+response = requests.get(url)
    
-# response.raise_for_status()
+response.raise_for_status()
  
-# dados = response.json()
+dados = response.json()
 
-# print (f"Dólar: R${dados["USDBRL"]["bid"]}")
-# print (f"Euro: R${dados["EURBRL"]["bid"]}")
-# print (f"Bitcon: R${dados["BTCBRL"]["bid"]}")
+print (f"Dólar: R${dados["USDBRL"]["bid"]}")
+print (f"Euro: R${dados["EURBRL"]["bid"]}")
+print (f"Bitcon: R${dados["BTCBRL"]["bid"]}")
 
 # CONVERTENDO EM REAIS PARA...
-url = "https://economia.awesomeapi.com.br/json/last/BRL-USD,BRL-EUR,BRL-BTC" 
+url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL" 
 
 response = requests.get(url)
    
