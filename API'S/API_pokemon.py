4746f37e6532dc192197e545c618d2e47925c680
Isabelle Ferreira <isabellesilvamaximo@gmail.com>
2025-05-21 16:46:16 -0300

    API_pokemon.py

--- API'S/API_pokemon.py
+++ API'S/API_pokemon.py
@@ -26,6 +26,42 @@ print(f'pokemon: {nome.title()}') # .title () primeira letra maiuscula
 print(f'Peso: {peso} kg')
 print(f'Altura: {alt/10} cm')
 
+
+# separando em funções 
+import requests
+
+# nome do pokemon para a busca:
+pokemon = input('Digite o nome do pokemon: ')
+
+def api_url():
+    # url da api:
+    url = "https://pokeapi.co/api/v2/pokemon/"
+    return url
+
+def converte(url):
+    # metodo get na api:
+    response = requests.get(url)
+
+    # erro na requisição:
+    response.raise_for_status() 
+    # converter para json:
+    dados = response.json()
+    return dados
+
+def main(dados):
+    nome = dados["name"]
+    alt = dados["height"]
+    peso = dados["weight"]
+
+
+print(f'pokemon: {nome.title()}') # .title () primeira letra maiuscula
+print(f'Peso: {peso} kg')
+print(f'Altura: {alt/10} cm')
+
+# filtro para habilidades:
+for habilidade in dados["abilities"]:
+    print(f'Habilidades: {habilidade["ability"]["name"]}')
+
 # filtro para habilidades:
 for habilidade in dados["abilities"]:
     print(f'Habilidades: {habilidade["ability"]["name"]}')
