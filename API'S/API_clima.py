e835036d48117ee9376e84e83ba5d351ca8b3cc6
Isabelle Ferreira <isabellesilvamaximo@gmail.com>
2025-05-21 16:37:45 -0300

    API_clima.py

--- API'S/API_clima.py
+++ API'S/API_clima.py
@@ -24,6 +24,48 @@ dados = response.json()
 temperatura = dados["current_weather"] ["temperature"]
 velocidade = dados["current_weather"]["windspeed"]
 
+# separando em funções:
+
+import requests
+
+def solicitar():
+    # inputs de latitude e longitude:
+    latitude = float(input("Digite a latitude: "))
+    longitude = float(input("Digite a longitude: "))
+    return latitude, longitude
+
+def url(latitude, longitude):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
+    return url
+    
+def requisicao(url):
+    # request da api metodo get:    
+    response = requests.get(url)
+    # erro na requisição:
+    response.raise_for_status()
+    # converter para json:
+    dados = response.json()
+    return dados
+
+def extrair_dados(dados):
+    # extrair dados:
+    temperatura = dados["current_weather"]["temperature"]
+    velocidade = dados["current_weather"]["windspeed"]
+    return temperatura, velocidade
+
+def main(temperatura, velocidade):
+    # exibir valores:
+    print(f"A temperatura atual é: {temperatura}C°")
+    print(f"A velocidade do vento atual é: {velocidade}km/h")
+
+if __name__ == "__main__":
+    # rodar o código:
+    latitude, longitude = solicitar()
+    url_api = url(latitude, longitude)
+    dados = requisicao(url_api)
+    temperatura, velocidade = extrair_dados(dados)
+    main(temperatura, velocidade)
+
 # exibir valores:
 print(f"A temperatura atual é: {temperatura}C°")
 print(f"A velocidade do vento atual é: {velocidade}km/h")
