2fd36e56014b19bf3f0eed7a4c3b1c66fae63d71
Isabelle Ferreira <isabellesilvamaximo@gmail.com>
2025-05-21 16:37:18 -0300

    API _cep.py

--- API'S/API _cep.py
+++ API'S/API _cep.py
@@ -25,3 +25,32 @@ print(f"Cidade: {dados['localidade']}")
 print(f"Estado: {dados['estado']}")
 print(f"UF: {dados['uf']}")
 print(f"Região: {dados['regiao']}")
+
+# separando em funções:
+import requests 
+
+def solicitar():
+    cep = input('Digite o cep: ')
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+    return url
+
+def requisiçao(url):
+    response = requests.get(url) 
+    response.raise_for_status()
+    dados = response.json()
+    return dados
+
+
+def resultado(dados):
+    print(f"Endereço: {dados['logradouro']}")
+    print(f"Bairro: {dados['bairro']}")
+    print(f"Cidade: {dados['localidade']}")
+    print(f"Estado: {dados['estado']}")
+    print(f"UF: {dados['uf']}")
+    print(f"Região: {dados['regiao']}")
+
+
+if __name__ == "__main__": # isso serve para rodar o código 
+    url = solicitar()
+    dados = requisiçao(url)
+    resultado(dados)
