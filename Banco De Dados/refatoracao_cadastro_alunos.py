d62c2216b95d7d27cbbeabf2b8ace3a49aee2eb5
ISABELLE LEITE DA SILVA MAXIMO FERREIRA <24271526@cfp107_2.local>
2025-05-22 15:28:32 -0300

    refatorando

--- Banco De Dados/refatoracao_cadastro_alunos.py
+++ Banco De Dados/refatoracao_cadastro_alunos.py
@@ -9,13 +9,17 @@ def validar_nome():
             
 def validar_nota():
     notas = []
-    for i in range (1,3):
-        nota = float(input(f"Digite a nota {i} entre 0 a 10: "))
-        if nota == 0 or nota <= 10:
-            notas.append(nota)
-            break
-        else:
-            print("Nota inválida. Deve estar entre 0 e 10!")
+    for i in range(1, 4):
+        while True:
+            try:
+                nota = float(input(f"Digite a nota {i} entre 0 a 10: "))
+                if 0 <= nota <= 10:
+                    notas.append(nota)
+                    break
+                else:
+                    print("Nota inválida. Deve estar entre 0 e 10!")
+            except ValueError:
+                print("Por favor, digite um número válido.")
     return notas
 
 def calculo_media(notas):
@@ -38,8 +42,8 @@ def cadastro_aluno(alunos):
         })
         
         # encontrar sistema ou sair 
-        continuar = input("Digite "S" para sair ou qualquer tecla para continuar").lower()
-        if continuar == "S":
+        continuar = input("Digite 's' para sair ou qualquer tecla para continuar: ").lower()
+        if continuar == "s":
             print("Saindo do sistemas...")
             break
         
@@ -52,9 +56,78 @@ def exibir_aluno(alunos):
             print(f""" 
                   Nome: {i["Nome"]}
                   Notas: {i["Notas"]}
-                  Médoa: {i["Media"]}
+                  Média: {i["Media"]}
                   Situação: {i["Situação"]}
                   """)
             print("-"*30)
             
+
+def editar_aluno(alunos):
+    nome = input("Digite o nome do aluno que deseja editar: ").lower()
+    for i in alunos: # loop para verificar o nome do input na lista
+        if i ["nome"].lower() == nome:
+            print (f"Notas atuais: {i["Notas"]}")
+            novas_notas = validar_nota()
+            media, situacao = calculo_media(novas_notas)
+            # add no dicionario :
+            {i["Notas"]} == novas_notas
+            {i["Media"]} == media
+            {i["Situação"]} == situacao
+            
+            print(f"Notas do aluno {i["nome"]} atualizadas com sucesso!")
+        
+        else:
+            print(f"Nome do aluno: {nome} não encontradi.")
+            
+# alunos aprovados e reprovados:
+# criando filtro 
+def exibir_situacao(alunos, situacao):
+    # lista para armazenar o filtro dos alunos 
+    filtros = [
+        # loop para exibir a situação:
+        i for i in alunos
+        # if p puxar a chave "Situação" do dicionario:
+        if i["Situação"] == situacao ]
+    
+    # exibir quando n tem alunos:
+    if not filtros:
+        print("Nenhum aluno encontrado!")
+    else:
+        for i in filtros:
+            print(f"Nome:{i["nome"]}, Situação:{i["Situação"]} ")
+            
+            
+def menu():
+    print(""" Menu de Opções
+          1 - Cadastrar alunos.
+          2 - Exibir alunos.
+          3 - Editar as notas de um aluno.
+          4 - Exibir alunos aprovados.
+          5 - Exibir alunos reprovados.
+          6 - Sair do sistema
+          """)
+    opcao = input("Escolha uma opção: ")
+    return opcao
+
+def main():
+    alunos = []
+    while True:
+        opcao = menu()
+        if opcao == "1":
+            cadastro_aluno(alunos)
+        elif opcao == "2":
+            exibir_aluno(alunos)
+        elif opcao == "3":
+            editar_aluno(alunos)
+        elif opcao == "4":
+            exibir_situacao(alunos, "Aprovado")
+        elif opcao == "5":
+            exibir_situacao(alunos, "Reprovado")
+        elif opcao == "6":
+            print("Saindo do sistemas...")
+        else:
+            print("Opção inválida, escolha uma opção existente.")
+            
+if __name__ == "__main__":
+    main()                
     
