1c6f4c3bd8c1343eb1171492f6db5a58f83f76ca
ISABELLE LEITE DA SILVA MAXIMO FERREIRA <24271526@cfp107_2.local>
2025-05-26 16:50:39 -0300

    atualização

--- API'S/api_cep/solicitação.py
+++ API'S/api_cep/solicitação.py
@@ -1,9 +1,17 @@
 
 import requests
 def solicitar(): 
-    try:
-        cep = input('Digite o cep: ')
-        url = f'https://viacep.com.br/ws/{cep}/json/'
-        return url
-    except ValueError:
-        print("Insira um cep válido!")
+    while True:
+        try:
+            cep = input("Digite o cep ou 's' para sair: ")
+            if cep.lower() == 's':
+                print("Saindo...")
+                break
+            elif not cep.isdigit() or len(cep) != 8:
+                print("CEP inválido. Deve conter 8 dígitos.")
+            else:
+                break
+        except ValueError:
+            print("Entrada inválida. Tente novamente.")
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+    return url
--- API'S/api_cep/whole.py
+++ API'S/api_cep/whole.py
@@ -33,7 +33,18 @@ print(f"Região: {dados['regiao']}")
 import requests 
 
 def solicitar(): 
-    cep = input('Digite o cep: ')
+    while True:
+        try:
+            cep = input("Digite o cep ou 's' para sair: ")
+            if cep.lower() == 's':
+                print("Saindo...")
+                break
+            elif not cep.isdigit() or len(cep) != 8:
+                print("CEP inválido. Deve conter 8 dígitos.")
+            else:
+                break
+        except ValueError:
+            print("Entrada inválida. Tente novamente.")
     url = f'https://viacep.com.br/ws/{cep}/json/'
     return url
 
