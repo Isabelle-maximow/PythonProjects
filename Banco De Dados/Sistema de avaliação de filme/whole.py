14e9d20147bb6c751111dd991ba98a8eed412028
ISABELLE LEITE DA SILVA MAXIMO FERREIRA <24271526@cfp107_2.local>
2025-05-30 16:36:49 -0300

    atualizações

--- Banco De Dados/Sistema de avaliação de filme/whole.py
+++ Banco De Dados/Sistema de avaliação de filme/whole.py
@@ -55,7 +55,7 @@ def validar_login(nome, senha):
         if conexao:
             # consulta no banco de dados:
             cursor.execute('''
-                           SELECT nome, senha FROM users WHERE nome = %s, senha = %s
+                           SELECT nome, senha FROM users WHERE nome = %s AND senha = %s
                            ''', (nome, senha))
             usuarios = cursor.fetchone() # traz linha por linha o banco
             # retornar o verdadeiro se oo usuario existir, falso caso contrario:
@@ -146,7 +146,7 @@ def tela_login():
     st.subheader("Digite suas credenciais")
     
     nome_usuarios = st.text_input("Nome do usuário: ")
-    senha = st.text_input("Senha: ", type: "password")
+    senha = st.text_input("Senha: ", type: "password" )
     
     if st.button("Entrar"):
         if validar_login(nome_usuarios,senha):
@@ -160,5 +160,80 @@ def tela_login():
         else:
             st.error("Usuário ou senha incorreto!")
             
+# sistema principal 
+def sistema_princiapl():
+    st.title("Gerenciador de filmes")
+    st.write(f"Usuário logado:{st.session_state["usuario"]}")
+    # botão de logout da barra latereal:
+    if st.sidebar.button("Sair"):
+        st.session_state["logado"] = False # deslogar usuario
+        del st.session_state["usuario"] # remover o nome do usuario da sessaõ
+        st.rerun()
+        
+    # abas para organizar as funcionalidades:
+    tab1, tab2, tab3, tab4 = st.tabs([
+        "Adicionar", "Atualizar", "Deletar", "Visualizar"
+    ])
+    # aba1: add filmes:
+    with tab1:
+        st.subheader("Adicionar filmes") 
+        nome = st.text_input ("Nome do filme: ")
+        ano = st.number_input ("Ano do filme: " min_value = 1900, max_value = 2100, step = 1)
+        nota = st.number_input("Nota: ", min_value = 0.0, max_value = 10.0, step = 0.5)
+        if st.button("Adicionar"):
+            # verificar se os campos estao preenchidos:
+            if nome and ano and nota is not None:
+                inserir_filme (nome, nota, ano)
+            else:
+                st.error("Preencha todos os campos!")
+                
+    # aba2: atualizar notas:
+    with tab2:
+        st.header("Atualizar notas")
+        filmes = ler_filme()
+        if filmes:
+            # criar uma lista de opções para a selectbox:
+            opcoes = [f""" {i[0]}  {i[1]} Nota - {i[2]}""" for i in filmes]
+            filme_selecionado = st.selectbox("Selecione o filme: ", opcoes )
+            nova_nota = st.number_input("Nota: ", min_value = 0.0, max_value = 10.0, step = 0.5)
+            if st.button("Atualizar"):
+                # id do filme, obter o index dele 
+                id_filme = filmes[opcoes.index(filme_selecionado)][0] # fixa o id 0 para comnparar
+                atualizar_filmes(id_filme, nova_nota)
+        else:
+            st.write("Nenhum filme cadastrado")
+            
+    # deletar filme
+    with tab3:
+        st.header("Deletar filme")
+        filmes = ler_filme()
+        if filmes:
+            # criar uma lista de opções para a selectbox:
+            opcoes = [f""" {i[0]}  {i[1]} Nota - {i[2]}""" for i in filmes]
+            filme_selecionado = st.selectbox("Selecione o filme: ", opcoes )
+            if st.button("Deletar"):
+                # id do filme, obter o index dele 
+                id_filme = filmes[opcoes.index(filme_selecionado)][0] # fixa o id 0 para comnparar
+                deletar_filme(id_filme)
+        else:
+            st.write("Nenhum filme cadastrado")
+            
+    # exibir filmes
+    with tab4:
+        st.header("Filmes cadastrados")
+        filmes = ler_filme()
+        exibir_grafico(filmes)
+
+def main():
+    if "logado" not in st.session_state:
+        st.session_state ["logado"] = False
+    # tela de login ou sistema princiupal:
+    if not st.session_state["logado"]:
+        tela_login() # mostra a tela de login
+    else:
+        sistema_princiapl() # sistema princiapl
     
-    
+# executar
+if __name__ == "__main__":
+    main()
+        
